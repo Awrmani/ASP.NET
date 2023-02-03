ca8979de07e8327d7b3c375ba5e48d440eab727b
Arman Valaee Asr <85237160+Awrmani@users.noreply.github.com>
2023-02-02 21:49:59 -0500

    Add files via upload

---
+++ MusicBiz/F2022A5AVA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="F2022A5AVA.MvcApplication" Language="C#" %>
---
+++ MusicBiz/F2022A5AVA/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(F2022A5AVA.Startup))]
+
+namespace F2022A5AVA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
